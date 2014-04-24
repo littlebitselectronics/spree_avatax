6f07d176ca478dda1405725c39cc62c34ed0bb98
Jonathan Tapia <jonathan.tapia@crowdint.com>
2014-04-24 14:34:09 -0500

    Add validation for gift_card

--- app/models/spree/line_item_decorator.rb
+++ app/models/spree/line_item_decorator.rb
@@ -2,8 +2,7 @@ module Spree
   LineItem.class_eval do
 
     def taxable?
-      return !self.tax_category.nil?
-      return !self.is_gift_card?
+      self.tax_category.nil? or self.is_gift_card? ? false : true
     end
 
   end
