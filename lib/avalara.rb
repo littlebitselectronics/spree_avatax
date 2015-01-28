d0422d0df016ce77a1e14fee3de8eceb9ff82d96
Fernando Barajas <fernando.barajas@crowdint.com>
2015-01-28 12:20:03 -0600

    - Refactor address match method

--- lib/avalara.rb
+++ lib/avalara.rb
@@ -147,10 +147,11 @@ module Avalara
 
   def self.address_match? address, response_address
     state = ((address.state && address.state.abbr) || (address.state_name || ''))
-    response_address["City"].downcase.eql?(address.city.downcase) &&
-      response_address["Region"].downcase.eql?(state.downcase) &&
-      response_address["PostalCode"].split('-').first.downcase.eql?(address.zipcode.downcase) &&
-      response_address["Country"].downcase.eql?(address.country.iso.downcase)
+    response_address.each { |k,v| v.downcase! }
+    response_address["City"].eql?(address.city.downcase) &&
+      response_address["Region"].eql?(state.downcase) &&
+      response_address["PostalCode"].split('-').first.eql?(address.zipcode.downcase) &&
+      response_address["Country"].eql?(address.country.iso.downcase)
   end
 
   def self.set_address_params address
